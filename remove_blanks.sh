c0eb6e6331f887f66301f8d558cc31909724a523
Amanda <anordwall03@gmail.com>
2025-02-12 15:22:33 -0500

    adding second lab 3 script

---
+++ remove_blanks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#have bash stop if errors
+
+set -e
+
+# set parameters
+
+LOCAL_CSV=$1
+
+NAME=$2
+
+#removing blanks
+
+echo "removing blanks and renaming file"
+
+# tr can remove spaces
+/usr/bin/cat "$LOCAL_CSV" | tr -s '\n' > "$NAME" ||exit 1
+
+echo "done! clean filed named $NAME"
